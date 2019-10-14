8160c85234582550ea4ec0b9c8933b9b9332413d
Dmitry Verteyko <verteyko1990@gmail.com>
2019-10-14 23:20:36 +0300

    form section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
 // JS
 import './js/'
+import 'jquery';
+import 'slick-carousel'
 
 // SCSS
 import './assets/scss/core.scss'
 require('font-awesome/css/font-awesome.css');
+require('font-awesome/css/font-awesome.css');
+import $ from "jquery";
+require('slick-carousel/slick/slick.scss');
+require('slick-carousel/slick/slick-theme.scss');
+require('slick-carousel/slick/slick-theme.scss');
 
 
-
+$('.lp-review-slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesPerRow: 2,
+    row: 2,
+    dotsClass: 'slick-dots lp-review-slider__dots',
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                centerMode: true,
+                centerPadding: '20px',
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1023,
+            settings: {
+                slidesToShow: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                infinite: true,
+                dots: true,
+                arrows: false,
+            }
+        },
+    ]
+});
