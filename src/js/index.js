8493844b4177c3970f6fc4a517f545354ed6d06c
Dmitry Verteyko <verteyko1990@gmail.com>
2019-10-17 21:33:56 +0300

    sessionStorage

--- src/js/index.js
+++ src/js/index.js
@@ -34,36 +34,24 @@ document.addEventListener("DOMContentLoaded", () => {
         showMenu.classList.toggle('active');
         line.classList.toggle('open');
     });
-
-
-    name.addEventListener('change',function (e) {
-        localStorage.setItem('name',e.target.value);
-    });
-
-    name.getAttribute('value',localStorage.getItem('name'))
-
-    phone.addEventListener('change',function (e) {
-        localStorage.setItem('phone',e.target.value);
-    });
-
-    phone.getAttribute('value',localStorage.getItem('phone'))
-
-
 });
 
 
-document.addEventListener("DOMContentLoaded", function() { // событие загрузки страницы
+document.addEventListener("DOMContentLoaded", function (key) { // событие загрузки страницы
 
-    // выбираем на странице все элементы типа textarea и input
-    document.querySelectorAll('textarea, input').forEach(function(e) {
-        // если данные значения уже записаны в sessionStorage, то вставляем их в поля формы
-        // путём этого мы как раз берём данные из памяти браузера, если страница была случайно перезагружена
-        e.value = window.sessionStorage.getItem(e.name, e.value);
-        // на событие ввода данных (включая вставку с помощью мыши) вешаем обработчик
-        e.addEventListener('input', function() {
-            // и записываем в sessionStorage данные, в качестве имени используя атрибут name поля элемента ввода
-            window.sessionStorage.setItem(e.name, e.value);
-        })
+    document.querySelectorAll('.name, .phone').forEach(function(e) {
+        if(e.classList.contains('name')) {
+            e.value = window.sessionStorage.getItem(e.name, e.value);
+                e.addEventListener('input', function() {
+                    window.sessionStorage.setItem(e.name, e.value);
+                })
+        }
+        else if (e.classList.contains('phone')) {
+            e.value = window.sessionStorage.getItem(e.phone, e.value);
+            e.addEventListener('input', function() {
+                window.sessionStorage.setItem(e.phone, e.value);
+            })
+        }
     })
 
 });
